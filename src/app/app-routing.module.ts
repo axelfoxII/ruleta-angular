d340e61a61e7802692a5ea4284a63d91f1d87128
Alex Salvatierra <asfoxii@gmail.com>
2021-02-06 01:57:25 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RuletaComponent } from './pages/ruleta/ruleta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'login', component: LoginComponent },
+  { path: 'ruleta', component: RuletaComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RuletaComponent } from './pages/ruleta/ruleta.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RuletaComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
